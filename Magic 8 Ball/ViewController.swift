059980a29ccd58c1a7455dc65b777991791ca71c
Omar Gonzalez <omargb22@gmail.com>
2019-05-06 22:11:02 -0700

    added motion sensor and ask button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,15 +13,26 @@ class ViewController: UIViewController {
     let ballArray = ["ball1","ball2", "ball3","ball4","ball5"]
     var randomBallNumber: Int = 0
     @IBOutlet weak var ImageView: UIImageView!
-    @IBOutlet weak var askButtonPressed: UIButton!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newBallImage()
+    }
+    
+    @IBAction func askButtonPress(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
         randomBallNumber = Int.random(in: 0 ... 4)
         ImageView.image = UIImage(named: ballArray[randomBallNumber])
     }
-    
     
 
 }
